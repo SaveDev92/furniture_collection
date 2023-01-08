0c63e63f2979f875be9821ced0931be2ae1ed18f
SaveDev92 <evdokimov-p92@yandex.ru>
2023-01-08 23:17:33 +0300

    add top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
 $(function () {
-
-  
+    $('.top__slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      arrows:false
+      });
 })
