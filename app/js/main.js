3bbe59d0a129fac194677691cf87ac2298611acb
SaveDev92 <evdokimov-p92@yandex.ru>
2023-01-11 11:38:54 +0300

    add block Gallery(btns, imgs) // add plugin mixinup // add js code with gallery block

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,21 @@ $(function () {
       adaptiveHeight: true,
       arrows:false
       });
+
+})
+
+let mixer = mixitup('.gallery__list');
+
+let btn1 =document.querySelector('.gallery__link1');
+btn1.click();
+
+
+let allBtns = document.querySelectorAll('.btns');
+
+allBtns.forEach(function(btn) {
+
+  btn.addEventListener('click', () => {
+    allBtns.forEach(e => e.classList.remove('active'));
+    btn.classList.add('active');
+    });
 })
